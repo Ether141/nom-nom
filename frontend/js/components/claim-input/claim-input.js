127fe82287ed668eb09774fb2920b7fb062b7728
Ether141 <42118307+Ether141@users.noreply.github.com>
2025-04-07 22:48:47 +0200

    Enhance claim input component with readonly state and validation methods

--- frontend/js/components/claim-input/claim-input.js
+++ frontend/js/components/claim-input/claim-input.js
@@ -12,7 +12,6 @@ export default class ClaimInput extends HTMLElement {
         this.type = this.getAttribute('type') || 'text';
         this.label = this.getAttribute('label') || '';
         this.iconSrc = this.getAttribute('iconSrc') || '';
-        this.errorText = this.getAttribute('errorText') || '';
         this.autocomplete = this.getAttribute('autocomplete') || 'off';
 
         this.attachShadow({ mode: 'open' });
@@ -25,16 +24,25 @@ export default class ClaimInput extends HTMLElement {
                     <label for="${this.inputId}" class="claim-input-label">${this.label}</label>
                     <img class="claim-input-icon" src="${this.iconSrc}" />
                 </div>
-                <span>${this.errorText}</span>
+                <span></span>
             </div>
         `;
 
+        this.rootElement = this.shadowRoot.querySelector('.claim-input-container');
         this.input = this.shadowRoot.querySelector('.claim-input-field');
 
         this.input.addEventListener('input', () => {
             this.value = this.input.value;
             this.dispatchEvent(new CustomEvent('change', { detail: this.input.value }));
         });
+
+        if (this.getAttribute("value")) {
+            this.value = this.getAttribute("value");
+        }
+        
+        if (this.hasAttribute('lock')) {
+            this.lock();
+        }
     }
 
     get value() {
@@ -44,4 +52,25 @@ export default class ClaimInput extends HTMLElement {
     set value(val) {
         this.input.value = val;
     }
+
+    invalid(text) {
+        this.shadowRoot.querySelector('.claim-input-container > span').innerHTML = text;
+        this.rootElement.classList.add('claim-input-invalid');
+    }
+
+    valid() {
+        this.rootElement.classList.remove('claim-input-invalid');
+    }
+
+    lock() {
+        this.input.disabled = true;
+        this.rootElement.classList.add('readonly');
+    }
+
+    unlock() {
+        this.input.disabled = false;
+        this.rootElement.classList.remove('readonly');
+    }
 }
+
+customElements.define('x-claim-input', ClaimInput);
