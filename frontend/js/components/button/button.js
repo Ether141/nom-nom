d1c6ce04c1a5f19057c10882c10e0792e9e485bc
Ether141 <42118307+Ether141@users.noreply.github.com>
2025-06-08 19:24:30 +0200

    Improvements in button component

--- frontend/js/components/button/button.js
+++ frontend/js/components/button/button.js
@@ -1,38 +1,55 @@
 export default class Button extends HTMLElement {
     constructor() {
         super();
-
         this.enabled = true;
         this.cssFile = '/js/components/button/button.css';
+        this._handleClick = this._handleClick.bind(this);
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
         this.label = this.getAttribute('label') || 'Button';
         this.buttonStyle = this.getAttribute('buttonStyle') || 'standard';
-
-        const style = this.buttonStyle === 'ghost' ? 'ghost-button' : 'standard-button';
-
-        this.attachShadow({ mode: 'open' });
+        const styleClass = this.buttonStyle === 'ghost' ? 'ghost-button' : 'standard-button';
 
         this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="${this.cssFile}">
-            <button class="${style}">${this.label}</button>
+            <button class="${styleClass}">${this.label}</button>
         `;
 
-        this.shadowRoot.querySelector('button').addEventListener('click', (event) => {
-            if (this.enabled) {
-                this.dispatchEvent(new CustomEvent('onclick', {
-                    bubbles: true,
-                    composed: true
-                }));
-            }
-        });
+        const buttonElement = this.shadowRoot.querySelector('button');
+        buttonElement.addEventListener('click', this._handleClick);
+    }
+
+    disconnectedCallback() {
+        const buttonElement = this.shadowRoot.querySelector('button');
+        if (buttonElement) {
+            buttonElement.removeEventListener('click', this._handleClick);
+        }
+    }
+
+    _handleClick(event) {
+        if (this.enabled) {
+            this.dispatchEvent(new CustomEvent('onclick', {
+                bubbles: true,
+                composed: true
+            }));
+        }
     }
 
     disable() {
-        this.shadowRoot.querySelector('button').classList.add('disabled');
+        const buttonElement = this.shadowRoot.querySelector('button');
+        if (buttonElement) {
+            buttonElement.classList.add('disabled');
+        }
         this.enabled = false;
     }
 
     enable() {
-        this.shadowRoot.querySelector('button').classList.remove('disabled');
+        const buttonElement = this.shadowRoot.querySelector('button');
+        if (buttonElement) {
+            buttonElement.classList.remove('disabled');
+        }
         this.enabled = true;
     }
 }
