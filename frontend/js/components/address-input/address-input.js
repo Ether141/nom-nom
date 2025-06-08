fed54c8f574d64d1c6aaf489489c36f248af509d
Ether141 <42118307+Ether141@users.noreply.github.com>
2025-06-08 19:23:25 +0200

    Handling events in address-input

--- frontend/js/components/address-input/address-input.js
+++ frontend/js/components/address-input/address-input.js
@@ -20,6 +20,24 @@ export default class AddressInput extends HTMLElement {
             this.value = this.input.value;
             this.dispatchEvent(new CustomEvent('change', { detail: this.input.value }));
         });
+
+        this.input.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                this.#searchAddress(this.input.value);
+                event.preventDefault();
+            }
+        });
+
+        this.shadowRoot.querySelector('.address-search-button').addEventListener('click', (event) => {
+            this.#searchAddress(this.input.value);
+            event.preventDefault();
+        });
+    }
+
+    #searchAddress(address) {
+        const encodedAddress = encodeURIComponent(address);
+        const searchUrl = `/pages/search.html?address=${encodedAddress}`;
+        window.location.href = searchUrl;
     }
 
     get value() {
