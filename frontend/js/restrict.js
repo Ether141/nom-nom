3800e38d4c4a17cda33b1d7b8050c9ec4ec91eb5
Ether141 <42118307+Ether141@users.noreply.github.com>
2025-06-11 21:30:06 +0200

    Add support for use roles in restrict.js

--- frontend/js/restrict.js
+++ frontend/js/restrict.js
@@ -4,12 +4,14 @@ function redirectToLogin() {
     window.location.href = '/pages/signin.html';
 }
 
-export function checkAccess() {
+export function checkAccess(role = null) {
     const client = new ApiClient();
 
     client.fetchUserInfo().then(userInfo => {
         if (!userInfo) {
             redirectToLogin();
+        } else if (role && userInfo.role !== role) {
+            window.location.href = '/index.html';
         }
     }).catch(() => {
         redirectToLogin();
